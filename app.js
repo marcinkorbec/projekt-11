2aa55055333aa97508020b6f7088604b293f7156
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-08-10 14:32:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-
-function clock() {
-  const time = new Date();
-  const seconds = time.getSeconds() < 10? '0' + time.getSeconds() : time.getSeconds();
-  const minutes = time.getMinutes() < 10? '0' + time.getMinutes() : time.getMinutes();
-  const hours = time.getHours() < 10? '0' + time.getHours() : time.getHours();
-  document.querySelector('.clock span').textContent = `${hours}:${minutes}:${seconds}`
+{
+  function clock() {
+    const time = new Date();
+    const seconds = time.getSeconds() < 10? '0' + time.getSeconds() : time.getSeconds();
+    const minutes = time.getMinutes() < 10? '0' + time.getMinutes() : time.getMinutes();
+    const hours = time.getHours() < 10? '0' + time.getHours() : time.getHours();
+    document.querySelector('.clock span').textContent = `${hours}:${minutes}:${seconds}`
+  }
 }
-
 setInterval(clock, 500);
 
 //-----------------------------------------------------------------------------------------------
-
-const nowTime = new Date().getTime();
-const endTime = new Date()
+{
+  const endTime = new Date('2021-12-31 23:59:59').getTime();
+  setInterval(() => {
+    const nowTime = new Date().getTime();
+    const time = Math.floor((endTime - nowTime)/1000);
+    console.log(time);
+  })
+}
