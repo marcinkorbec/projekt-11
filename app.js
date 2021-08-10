09f0eb93f209120326f3acf57eae31a1bf9c9af6
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-08-10 15:23:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -12,12 +12,21 @@ setInterval(clock, 500);
 //-----------------------------------------------------------------------------------------------
 {
   const endTime = new Date('2021-12-31 23:59:59').getTime();
-  const spanD = document.querySelector('span.d')
+  const spanD = document.querySelector('span.d');
+  const spanH = document.querySelector('span.h');
+  const spanM = document.querySelector('span.m');
+  const spanS = document.querySelector('span.s');
 
   setInterval(() => {
     const nowTime = new Date().getTime();
     const time = endTime - nowTime;
     const days = Math.floor((endTime/(1000*60*60*24)) - (nowTime/(1000*60*60*24)));
-    spanD.textContent=`${days} dni,`;
+    const hours = Math.floor((endTime/(1000*60*60) - nowTime/(1000*60*60)) % 24);
+    const minutes = Math.floor((endTime/(1000*60*60) - nowTime/(1000*60*60)) % 60);
+    const seconds = Math.floor((endTime / 1000 - nowTime / 1000) % 60);
+    spanD.textContent =`${days} dni,`;
+    spanH.textContent = `${hours} godzin,`;
+    spanM.textContent = `${minutes} minut,`;
+    spanS.textContent = `${seconds} sekund.`;
   })
 }
